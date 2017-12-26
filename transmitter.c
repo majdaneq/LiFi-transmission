9fef4a635b9b82aa739f0515bbc7b700ed9c5347
Szymon Bortel <szymonbortel8@gmail.com>
2017-12-26 19:48:25 +0100

    98uuj
    98ju

--- transmitter.c
+++ transmitter.c
@@ -4,35 +4,108 @@
 #include <time.h>
 #include <stdlib.h>
 
-
+const uint32_t MaskLED = {1UL << 5};
+const static uint32_t transmask2 = {1UL << 3};
 int tab[1000];
-	int i=0;
+int ramka_tab[20];
+int i=0;
+	
+	
 void initialize_pin(void) {
 	SIM->SCGC5 |= (SIM_SCGC5_PORTD_MASK);														
 	PORTD->PCR[3] = PORT_PCR_MUX(1UL);
 	PORTD->PCR[5] = PORT_PCR_MUX(1UL);
+	PORTD->PCR[1] = PORT_PCR_MUX(1UL);
+	PORTD->PCR[2] = PORT_PCR_MUX(1UL);
+	PORTD->PCR[4] = PORT_PCR_MUX(1UL);
+	PORTD->PCR[6] = PORT_PCR_MUX(1UL);
+	PORTD->PCR[7] = PORT_PCR_MUX(1UL);
+	PORTD->PCR[3] = PORT_PCR_MUX(1UL);
+	
 	PTD->PDDR |= transmask2;	
 	PTD->PSOR |= transmask2;
+
+		
+	
+	PTD->PDDR |= MaskLED;
+	PTD->PSOR |=MaskLED;
 }
 
-void filltab()
-{	for(i=0;i<1000;i++)
+void filltab()									//wypelnienie tablicy danych
+{	
+	for(i=0;i<1000;i++)
 	{tab[i]=rand()%2;		
 	}	
 }
 
+void ramkaopen()
+{
+	for(i=0;i<5;i++)
+	ramka_tab[i]=1;	
+	for(i=5;i<10;i++)
+	ramka_tab[i]=0;
+	for(i=10;i<15;i++)
+	ramka_tab[i]=1;	
+	for(i=15;i<20;i++)
+	ramka_tab[i]=0;
+}
+
+void ramkaclose()
+{
+	for(i=0;i<5;i++)
+	ramka_tab[i]=0;	
+	for(i=5;i<10;i++)
+	ramka_tab[i]=1;	
+	for(i=10;i<15;i++)
+	ramka_tab[i]=0;	
+	for(i=15;i<20;i++)
+	ramka_tab[i]=1;	
+}
+
 void togg(void)
-{	delay_mc(100);
+{	
+	delay_mc(100);
 	PTD->PTOR |=transmask2;
 }
 
 void senddata()
-{	for(i=0;i<1000;i++)
-	{	if (tab[i]==0)
-			PTD->PCOR |=transmask2;
-		else 
-			PTD->PSOR |=transmask2;
+{	
+	ramkaopen();
+	for (i=0;i<20;i++)											//start
+		{	
+			if (ramka_tab[i]==0)
+					PTD->PCOR |=transmask2;
+			else 
+					PTD->PSOR |=transmask2;
+			delay_mc(100);
+		}	
+		PTD->PCOR|=MaskLED;
+	for(i=0;i<1000;i++)											//dane
+		{	
+			if (tab[i]==0)
+				PTD->PCOR |=transmask2;
+			else 
+				PTD->PSOR |=transmask2;
+			
+			delay_mc(100);
+		}
+		ramkaclose();
+		PTD->PSOR|=MaskLED;
+		for (i=0;i<20;i++)										//stop
+		{	
+			if (ramka_tab[i]==0)
+					PTD->PCOR |=transmask2;
+			else 
+					PTD->PSOR |=transmask2;
+			delay_mc(100);
+		}
+	
+}
 
-			delay_mc(50);
+ 
+void delay_mc(uint32_t value){
+	uint32_t delay, x;	
+	for(x=0; x < value; x++){
+		for(delay=0; delay < 10000; delay++){};
 	}
 }
--- transmitter.h
+++ transmitter.h
@@ -2,11 +2,12 @@
 #define transmitter_h
 #include "MKL46Z4.h"
 
-const static uint32_t transmask2 = {1UL << 3};
+
 //const static uint32_t transmask2[] ={1UL << 5, 1UL << 3};
 void initialize_pin(void);
  void togg(void);
 void filltab(void);
 void senddata(void);
+void ramka(void);
 
 #endif
